4e1e94539d3acf089e236904ac557001d19e34ee
Medhansh Garg <medhansh2005@gmaill.com>
2025-07-02 16:45:21 +0530

    Added GoatCounter Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { Courier_Prime } from "next/font/google";
 import { Metadata } from "next";
+import { GCScript } from "next-goatcounter";
 
 export const metadata: Metadata = {
   title: {
@@ -17,6 +18,8 @@ const courier = Courier_Prime({ subsets: ["latin"], weight: ["400", "700"] });
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      {/* <script data-goatcounter="https://hackoverflow.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script> */}
+      <GCScript siteUrl={"https://hackoverflow.goatcounter.com/count"} />
       <body className={`bg-[#0f0f0f] scroll-smooth text-white ${inter.className}`}>{children}</body>
     </html>
   );
